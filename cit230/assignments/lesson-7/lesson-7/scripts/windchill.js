bffdf4d7f9cb172ca490b9c20f06fa106f0c8cfd
Melanie Rose <ros17025@byui.edu>
2018-06-16 21:46:33 -0500

    Update

--- cit230/assignments/lesson-7/lesson-7/scripts/windchill.js
+++ cit230/assignments/lesson-7/lesson-7/scripts/windchill.js
@@ -1,9 +1,17 @@
-var highTemp = 90;
-var lowTemp = 66;
+
+    
+var highTemp = document.getElementById("high").innerHTML; 
+var lowTemp = document.getElementById("low").innerHTML;
+
 var temp = (highTemp + lowTemp)/2;
-var s = 5
+var s = document.getElementById("wspeed").innerHTML;
 
 var x = Math.pow(s, 0.16);
 var windChill = parseInt(35.74 + (0.6215 * temp) - (35.75 * x) + (0.4275 * temp * x));
-windChill = Math.round(windChill*100)/100;
-document.getElementById("windchill").innerHTML = "Windchill" + ":" + " " + windChill + "&deg;F";
+windChill = Math.round(windChill * 100)/100;
+var wc = document.getElementById("windchill").innerHTML = "Windchill" + ":" + " " + windChill + "&deg;F";
+
+
+
+
+
